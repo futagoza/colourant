95f7b2ded9cb0c1518e8c90899e78233839c0a63
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2019-07-22 19:04:10 +0100

    Add support for TypeScript

--- ava.config.js
+++ ava.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "cache": true,
     "failFast": false,
     "failWithoutAssertions": false,
+    "compileEnhancements": false,
+    "extensions": [
+        "ts",
+    ],
+    "require": [
+        "ts-node/register",
+    ],
     "babel": false,
 
 };
